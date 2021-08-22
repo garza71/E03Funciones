74f50c2cad4a7334b4bab994a94b6e5679f85430
Lina Patricia Garza Gomez <57541431+garza71@users.noreply.github.com>
2021-08-22 16:59:48 -0500

    Update input_data.py

--- assignments/08TarjeteriaEspanola/tests/input_data.py
+++ assignments/08TarjeteriaEspanola/tests/input_data.py
@@ -9,18 +9,18 @@ input_values = [
     
     (
         ["7","2"],
-        ["Dame la cantidad de pliegos de papel albanene: ","Dame la cantidad de plumones: ","El número máximo de tarjetas que se pueden hacer es: 70"],
+        ["Dame la cantidad de pliegos de papel albanene: ","Dame la cantidad de plumones: ","El numero maximo de tarjetas que se pueden hacer es: 70"],
         " -->a) Revisa los tipos de dato. Revisa el número de tarjetas que puedes hacer con cada material."
     ),
     (
         ["3","3"],
-        ["Dame la cantidad de pliegos de papel albanene: ","Dame la cantidad de plumones: ","El número máximo de tarjetas que se pueden hacer es: 36"],
+        ["Dame la cantidad de pliegos de papel albanene: ","Dame la cantidad de plumones: ","El numero maximo de tarjetas que se pueden hacer es: 36"],
         " -->b) Revisa los tipos de dato. Revisa el número de tarjetas que puedes hacer con cada material."
     )
     ,
     (
         ["20","4"],
-        ["Dame la cantidad de pliegos de papel albanene: ","Dame la cantidad de plumones: ","El número máximo de tarjetas que se pueden hacer es: 140"],
+        ["Dame la cantidad de pliegos de papel albanene: ","Dame la cantidad de plumones: ","El numero maximo de tarjetas que se pueden hacer es: 140"],
         " -->c) Revisa los tipos de dato. Revisa el número de tarjetas que puedes hacer con cada material."
     )
 ]
