2e278aeaa878e30ac2bae9d2e445081cd31015cb
Lina Patricia Garza Gomez <57541431+garza71@users.noreply.github.com>
2021-08-22 17:20:11 -0500

    Update exercise.py

--- assignments/04TiendaSillas/src/exercise.py
+++ assignments/04TiendaSillas/src/exercise.py
@@ -1,37 +1,22 @@
 
 def precio_antes_descuento(tipo_silla, cantidad) :
-    if tipo_silla == "B" :
-        precio = 700.0 * cantidad
-    elif tipo_silla == "E" :
-        precio = 900.0 * cantidad
-    else :
-        precio = 1500.0 * cantidad
-    return precio
+    #La función calcula y retorna el total incial, antes del descuento.
 
 def calcula_descuento(precio, tipo_cl) :
-    if tipo_cl == "F" :
-        desc = precio * 0.2
-    else :
-        if precio >= 10000 and precio < 20000 :
-            desc = precio * 0.1
-        elif precio >= 20000 :
-            desc = precio * 0.15
-        else :
-            desc = 0.0
-    return desc
+    #La función calcula y retorna el monto del descuento.
+
 
 def main() :
-    # pido el tipo de silla (B E L)
-    tipo_silla = input("Tipo silla: ")
-    # pido el tipo de cliente (N F)
-    tipo_cl = input("Tipo cliente: ")
-    cantidad = int(input("Cantidad de sillas: "))
+    # pido el tipo de silla (solo acepta B, E o L): ("Tipo silla: ")
+    # pido el tipo de cliente (solo acepta N o F): ("Tipo cliente: ")
+    # pido la cantidad de sillas: ("Cantidad de sillas: "))
 
-    subtotal = precio_antes_descuento(tipo_silla, cantidad)
-    desc = calcula_descuento(subtotal, tipo_cl)
-    total = subtotal - desc
+    #Calcula el subtotal
+    #Calcula el descuento
+    #Calcula el total 
+    #Imprime
     print(f"Total sin dcto.  ${subtotal:>7}")
-    print(f"Descuento        ${desc:>7}")
+    print(f"Descuento        ${descuento:>7}")
     print(f"Total a pagar    ${total:>7}")
 
 
